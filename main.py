181ef30a46d36f5ed745954030a36df3d96dfb7b
Vasily <mr.kulak.06@mail.ru>
2025-09-16 13:19:08 +0300

    добавлены пароли

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sorts import *
 
 arr = [53,2,234,66,1,632,3,44,52]
 arr = insertion_sort(arr)
-print(arr)
 
 print(bin_search(arr,234, 0, len(arr)))
 
--- sorts.py
+++ sorts.py
@@ -1,15 +1,13 @@
 def insertion_sort(arr):
 
     for i in range(1, len(arr)):
-        key = arr[i]  # Текущий элемент для вставки
-        j = i - 1  # Индекс предыдущего элемента
+        key = arr[i]
+        j = i - 1
 
-        # Сдвигаем элементы больше key вправо
         while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]  # Сдвигаем элемент вправо
+            arr[j + 1] = arr[j]  #
             j -= 1
 
-        # Вставляем key на правильную позицию
         arr[j + 1] = key
 
     return arr
